5583f88cfbf1d709c8ce69b2c6eda44ce186bfe4
Dmitry Romanov <romanovda@gmail.com>
2021-01-07 10:55:36 -0500

    Examples and vscode files

---
+++ examples/g4e_pgun.py
@@ -0,0 +1,67 @@
+from g4epy import Geant4Eic
+
+g4e = Geant4Eic(beamline='erhic')
+
+# Select cone particle gun generator
+g4e.command('/generator/select coneParticleGun')
+
+# Set particle to be generated.
+# (geantino is default)
+# (ion can be specified for shooting ions)
+# Common names: proton, e- e+ pi+ pi-
+g4e.command('/generator/coneParticleGun/particle e-')
+
+# Set momentum direction
+# Direction needs not to be a unit vector
+g4e.command('/generator/coneParticleGun/direction 0 0 -1')
+
+# Set kinetic energy [GeV]
+g4e.command('/generator/coneParticleGun/energy 6 GeV')
+
+# Energy spread [GeV],
+# energy is smeared as gauss (mean=<particle E>, stddev=energyStdDev)
+g4e.command('/generator/coneParticleGun/energyStdDev 1 GeV')
+
+# Cone angle standard deviation.
+# Basically is the resulting cone angle
+g4e.command('/generator/coneParticleGun/coneAngleStdDev 0.3 rad')
+
+# Set number of particles to be generated
+g4e.command('/generator/coneParticleGun/number 1')
+
+# To control how many generation of secondaries (tracks and their hits) to save,
+# there is a configuration:
+#    /rootOutput/saveSecondaryLevel <ancestry-level>
+#
+# <ancestry-level> sets 0-n levels of ancestry which are saved in root file.
+#
+# Example:
+#
+# -1 - save everything
+# 0 - save only primary particles
+# 1 - save primaries and their daughters
+# 2 - save primaries, daughters and daughters’ daughters
+# n - save n generations of secondaries
+#
+# (primaries - particles that came from a generator/input file)
+#
+# The default level is 3, which corresponds to:
+#
+# /rootOutput/saveSecondaryLevel 3
+#
+# We set it to 1. If only vertex particles are of the interest, set it to 0
+#
+# This flag doesn't affect physics in g4e (only what is saved)
+# so EM showers in EMCAL is fully simulated with any value here
+g4e.command(['/rootOutput/saveSecondaryLevel 1'])
+
+# Extension is omitted here
+# g4e creates a bunch of files with this name and different extensions
+g4e.output('work/pgun_6GeV')
+
+# Run g4e run!!!
+#g4e.beam_on(300).run()
+
+
+# Run
+g4e.beam_on(2).run()
---
+++ examples/g4e_simulation.py
@@ -0,0 +1,43 @@
+from g4epy import Geant4Eic
+
+g4e = Geant4Eic(beamline='erhic')
+
+# Beagle provides beam energy information, but for the sake of simplicity we
+g4e.command(['/detsetup/eBeam 5',
+             '/detsetup/pBeam 50'
+             ])
+
+# To control how many generation of secondaries (tracks and their hits) to save,
+# there is a configuration:
+#    /rootOutput/saveSecondaryLevel <ancestry-level>
+#
+# <ancestry-level> sets 0-n levels of ancestry which are saved in root file.
+#
+# Example:
+#
+# -1 - save everything
+# 0 - save only primary particles
+# 1 - save primaries and their daughters
+# 2 - save primaries, daughters and daughters’ daughters
+# n - save n generations of secondaries
+#
+# (primaries - particles that came from a generator/input file)
+#
+# The default level is 3, which corresponds to:
+#
+# /rootOutput/saveSecondaryLevel 3
+#
+# We set it to 1. If only vertex particles are of the interest, set it to 0
+#
+# This flag doesn't affect physics in g4e (only what is saved)
+# so EM showers in EMCAL is fully simulated with any value here
+g4e.command(['/rootOutput/saveSecondaryLevel 1'])
+
+# Extension is omitted here
+# g4e creates a bunch of files with this name and different extensions
+g4e.output('work/g4e_emcal')
+
+# Run g4e run!!!
+g4e.source('work/beagle.txt') \
+   .beam_on(1000) \
+   .run()
